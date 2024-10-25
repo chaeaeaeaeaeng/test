34306545cfbdeb125281095a23d74da270676be4
채연 <qkrcodus0403@gmail.com>
2024-10-25 17:38:30 +0900

    네번째 커밋

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="p-10">
       <h1>
-        안녕하세요 반갑습니다
+        안녕하세요 반갑습니다 박채연입니다
       </h1>
       <a href="/test">테스트</a>
     </div>
